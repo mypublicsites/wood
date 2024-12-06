29e2a6d0194ef46cb6406d9dc3a47ca7d030b490
Arslan Ali <63331842+mypublicsites@users.noreply.github.com>
2024-12-06 16:50:28 +0000

    Update node.js

--- node.js
+++ node.js
@@ -1,10 +1,10 @@
-Function sendMail(){
-	let parms={
-		name : documet.getElementById("name").value,
-		email : document.getElementById("email").value,
-		message : document.getElementById("message").value,
+	Function sendMail(){
+		let parms={
+			name : documet.getElementById("name").value,
+			email : document.getElementById("email").value,
+			message : document.getElementById("message").value
+			
+		}	
 		
-	}	
-	
-emails.send("service_193f4gs", "template_bqx52qq",parms).then(alert("Email Sent"))	
-}
+	emailjs.send("service_193f4gs", "template_bqx52qq",parms).then(alert("Email Sent"))	
+	}
